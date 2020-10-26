431cb6ba934d19f554fce4266827fe06cda4a7b4
Ella Vankirk <ella.vankirk@gmail.com>
2020-10-26 00:30:20 -0500

    Removed some variables

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -3,11 +3,11 @@
 import { SpaceLocation } from './SpaceLocation';
 
 // Part 1: Declare (5) Variables With Type
-let spacecraftName: string = 'Determination';
-let speedMph: number = 17500;
+
+
 let kilometersToMars: number = 225000000;
 let kilometersToTheMoon: number = 384400;
-let milesPerKilometer: number =	0.621;
+
 
 
 
@@ -22,18 +22,9 @@ let daysToMars: number = hoursToMars / 24;
 
 
 // Part 3: Create a Function ("getDaysToLocation")
-function getDaysToLocation(kilometersAway: number): number {
-    let milesAway: number = kilometersAway * milesPerKilometer;
-    let hoursToLocation: number = milesAway / speedMph;
-    let daysToLocation: number = hoursToLocation / 24;
-    return daysToLocation;
-}
-
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
-
 
 
 // Part 4: Create a Spacecraft Class
@@ -69,8 +60,6 @@ let spaceShuttle = new Spacecraft('Determination', 17500);
 
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-// console.log(`It takes the ${spaceShuttle.name} ${spaceShuttle.getDaysToLocation(kilometersToMars)} days to reach Mars.`);
-// console.log(`It takes the ${spaceShuttle.name} ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} days to reach the Moon.`);
 
 
 // Part 5: Export and Import the SpaceLocation Class
